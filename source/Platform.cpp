9c521b95cc64fb4a88e5a8dd89e5b66fd228ace4
SkyGuy29 (Laptop) <christianmoegling360@gmail.com>
2025-02-27 13:04:09 -0600

    and i finished off the platform data. I really need to stop working on this I have other homework past do HELP

--- source/Platform.cpp
+++ source/Platform.cpp
@@ -20,11 +20,11 @@ Platform::Platform(int platformID, std::string file)
 		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(194 * WINDOW_SCALE, 91 * WINDOW_SCALE));
 		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(178 * WINDOW_SCALE, 91 * WINDOW_SCALE));
 		break;
-	case 2: //right side small (UNFINISHED)
-		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(65 * WINDOW_SCALE, 40 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(134 * WINDOW_SCALE, 40 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(130 * WINDOW_SCALE, 48 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(78 * WINDOW_SCALE, 48 * WINDOW_SCALE));
+	case 2: //right side small
+		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(201 * WINDOW_SCALE, 91 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(235 * WINDOW_SCALE, 91 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(220 * WINDOW_SCALE, 96 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(205 * WINDOW_SCALE, 96 * WINDOW_SCALE));
 		break;
 	case 3: //left side
 		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(0 * WINDOW_SCALE, 91 * WINDOW_SCALE));
@@ -38,11 +38,23 @@ Platform::Platform(int platformID, std::string file)
 		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(120 * WINDOW_SCALE, 120 * WINDOW_SCALE));
 		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(92 * WINDOW_SCALE, 120 * WINDOW_SCALE));
 		break;
-	case 5: //top left (UNFINISHED)
-		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(65 * WINDOW_SCALE, 40 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(134 * WINDOW_SCALE, 40 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(130 * WINDOW_SCALE, 48 * WINDOW_SCALE));
-		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(78 * WINDOW_SCALE, 48 * WINDOW_SCALE));
+	case 5: //top left
+		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(0 * WINDOW_SCALE, 29 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(23 * WINDOW_SCALE, 29 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(16 * WINDOW_SCALE, 33 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(0 * WINDOW_SCALE, 33 * WINDOW_SCALE));
+		break;
+	case 6: //top right
+		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(198 * WINDOW_SCALE, 29 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(235 * WINDOW_SCALE, 29 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(235 * WINDOW_SCALE, 33 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(204 * WINDOW_SCALE, 33 * WINDOW_SCALE));
+		break;
+	case 7: //ground
+		hitbox.setPoint(ConvexCorners::TOP_LEFT, sf::Vector2f(39 * WINDOW_SCALE, 157 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::TOP_RIGHT, sf::Vector2f(187 * WINDOW_SCALE, 157 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_RIGHT, sf::Vector2f(177 * WINDOW_SCALE, 169 * WINDOW_SCALE));
+		hitbox.setPoint(ConvexCorners::BOT_LEFT, sf::Vector2f(47 * WINDOW_SCALE, 169 * WINDOW_SCALE));
 		break;
 	}
 
