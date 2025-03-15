6e24df17602f934d62fccacb1cd700ac54759a1a
SkyGuy <christianmoegling360@gmail.com>
2025-03-14 21:43:56 -0500

    top center platform animation added this took FOR FREAKING EVER

--- source/SpriteData.h
+++ source/SpriteData.h
@@ -32,6 +32,10 @@ enum AnimationNames
     SHADOW_GROUND,
     SHADOW_FLY,
     TEMP_BACKGROUND,
+    PLAT_TOP_CENTER_ANIM,
+    PLAT_LEFT_ANIM,
+    PLAT_TOP_RIGHT_ANIM,
+    PLAT_CENTER_ANIM,
     DATA_COUNT //this helps make sure array size matches enum count
 };
 
@@ -58,6 +62,10 @@ static const SpriteData spriteData[AnimationNames::DATA_COUNT] =
     {"res/Art/enemyAnimations.png", sf::IntRect(0, 18, 13, 12), 2}, //HUNTER_FLY 
     {"res/Art/enemyAnimations.png", sf::IntRect(52, 0, 13, 18), 4}, //SHADOW_GROUND 
     {"res/Art/enemyAnimations.png", sf::IntRect(52, 18, 13, 12), 2},//SHADOW_FLY 
-    {"res/Art/joustSprite.png", sf::IntRect(2, 44, 237, 195), 1}    //TEMP_BACKGROUND *
+    {"res/Art/joustSprite.png", sf::IntRect(2, 44, 237, 195), 1},   //TEMP_BACKGROUND
+    {"res/Art/topCenterPlat.png", sf::IntRect(0, 0, 88, 9), 7},     //PLAT_TOP_CENTER_ANIM
+    {"res/Art/joustSprite.png", sf::IntRect(0, 0, 237, 195), 1},    //PLAT_TOP_LEFT_ANIM *
+    {"res/Art/joustSprite.png", sf::IntRect(0, 0, 237, 195), 1},    //PLAT_TOP_RIGHT_ANIM *
+    {"res/Art/joustSprite.png", sf::IntRect(0, 0, 237, 195), 1}     //PLAT_CENTER_ANIM *
     //* INDICATES UNFINISHED DO NOT USE
 };
