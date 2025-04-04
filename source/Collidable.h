9b858ba50e3ac62bc15ab312f96c4eb04a944237
SkyGuy29 <christianmoegling360@gmail.com>
2025-04-04 12:00:10 -0500

    some comments and things and such idk what to do

--- source/Collidable.h
+++ source/Collidable.h
@@ -6,12 +6,20 @@
 class Collidable
 {
 public:
+	//updates the collidable once, meant to be called every frame
 	virtual void update() {}
+	
 	virtual void setOnGround(float, int);
 	virtual void setOffGround() { currentPlatform = -1; }
 	virtual int getGrounded() { return currentPlatform; }
+
+	//flips the velocity of the collidable horizontally
 	virtual void bounceX() { vel.x *= -1; }
+
+	//flips the velocity of the collidable vertically
 	virtual void bounceY() { vel.y *= -1; }
+
+	//repositions the hitbox based on the formula given in res\misc\diagonalCollisionDerivationP2.png
 	virtual void bounceSetLeft(Platform);
 	virtual void bounceSetRight(Platform);
 	virtual void setPosition(const sf::Vector2f newPos) { hitbox.setPosition(newPos); }
