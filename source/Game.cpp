79740d20490913899ba57c59a84934ed63c754b2
SkyGuy29 (Laptop) <christianmoegling360@gmail.com>
2025-04-23 13:32:49 -0500

    i was off by 0.8 pixels

--- source/Game.cpp
+++ source/Game.cpp
@@ -22,11 +22,11 @@ Game::Game()
 	scoreText.setFillColor(sf::Color::Yellow);
 	scoreText.setString("0");
 	scoreText.setOrigin(scoreText.getLocalBounds().width / 2, 
-		scoreText.getLocalBounds().height / 2 - WINDOW_SCALE * 4); //four
+		scoreText.getLocalBounds().height / 2 + 4.8); //AS IF I KNOW WHY I NEED TO ADD EXACTLY 4.8 PIXELS
 	scoreText.setScale(WINDOW_SCALE, WINDOW_SCALE);
 	scoreText.setPosition(104 * WINDOW_SCALE, 213 * WINDOW_SCALE);
 
-	player[0].setPosition(sf::Vector2f(1000, 1000));
+	player[0].setPosition(sf::Vector2f(166 * WINDOW_SCALE, 166 * WINDOW_SCALE));
 }
 
 
@@ -112,8 +112,8 @@ void Game::update()
 	//std::cout << "ENVP: " << timer.restart().asMilliseconds() / 1000. << '\n';
 
 	scoreText.setString(std::to_string(score[0]));
-	//scoreText.setOrigin(scoreText.getLocalBounds().width - scoreText.getCharacterSize() / 2,
-	//	scoreText.getLocalBounds().height / 2);
+	scoreText.setOrigin(scoreText.getLocalBounds().width - scoreText.getCharacterSize() / 2,
+		scoreText.getLocalBounds().height / 2 + 4.8);
 }
 
 
@@ -163,9 +163,6 @@ void Game::drawTo(sf::RenderWindow& window)
 		i->drawTo(window);
 	player[0].drawTo(window);
 	window.draw(scoreText);
-
-	std::cout << sf::Mouse::getPosition(window).x / WINDOW_SCALE << " " << sf::Mouse::getPosition(window).y / WINDOW_SCALE << "    ";
-	std::cout << scoreText.getPosition().x / WINDOW_SCALE << " " << scoreText.getPosition().y / WINDOW_SCALE << std::endl;
 }
 
 
