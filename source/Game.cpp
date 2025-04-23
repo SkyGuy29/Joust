6e384620dd467c850a53f626c939050f98cc2dda
chalkEater999 <rowan.x.holland@gmail.com>
2025-04-23 11:33:36 -0500

    remove test egg

--- source/Game.cpp
+++ source/Game.cpp
@@ -9,7 +9,6 @@ Game::Game()
 	bridge.setPosition(0, platform[PlatformNames::P_GROUND].getPointPos(ConvexCorners::TOP_LEFT).y);
 	bridge.setFillColor(sf::Color(144, 72, 0));
 
-	eggVec.emplace_back(new Egg);
 	enemyVec.emplace_back(new Bounder);
 	enemyVec.emplace_back(new Hunter);
 	//enemyVec.emplace_back(new Hunter);
