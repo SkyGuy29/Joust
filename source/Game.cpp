79d6f6711fceb5930b48120e5b9fe4bdf5b0fc79
artzombie255 <artzombie255@gmail.com>
2025-04-23 11:39:32 -0500

    fixed the fix

--- source/Game.cpp
+++ source/Game.cpp
@@ -38,7 +38,7 @@ void Game::update()
 	player[0].update();
 	//std::cout << "update: " << timer.restart().asMilliseconds() / 1000. << '\n';
 
-	if (enemyVec.size() == 1)
+	if (enemyVec.size() > 1)
 	{
 		for (int i = 0; i < enemyVec.size() - 1; i++)
 			for (int j = i + 1; j < enemyVec.size(); j++)
