9b87587211e7140231c32b002269d4ac793207ec
chalkEater999 <rowan.x.holland@gmail.com>
2025-04-25 11:39:42 -0500

    eggs hatch

--- source/Egg.h
+++ source/Egg.h
@@ -6,10 +6,9 @@
 class Egg : public Collidable
 {
 public:
-	Egg();
+	Egg(EnemyTypes);
 	Egg(sf::Vector2f, sf::Vector2f, EnemyTypes);
 	void update() override;
-	void hatch();
 	int getTimer() const { return eggTimer.getElapsedTime().asMilliseconds(); }
 
 	EnemyTypes getType()
@@ -20,6 +19,7 @@ public:
 	void drawTo(sf::RenderWindow&);
 private:
 
+	void hatch();
 	EnemyTypes type;
 	sf::Clock eggTimer;
 	Animation sprite;
