9c6b531d7e8bd4f86ff853e0ff6cc46827fe6e7e
SkyGuy29 (Laptop) <christianmoegling360@gmail.com>
2025-05-05 13:15:20 -0500

    added a lava rectangle because i felt like it, where you die needs to be moved up

--- source/Game.cpp
+++ source/Game.cpp
@@ -15,6 +15,10 @@ Game::Game()
 	bridge.setPosition(0, platform[PlatformNames::P_GROUND].getPointPos(ConvexCorners::TOP_LEFT).y);
 	bridge.setFillColor(sf::Color(144, 72, 0));
 
+	lava.setSize(sf::Vector2f(WINDOW_X * WINDOW_SCALE, 26 * WINDOW_SCALE)); //numbers from sprite data
+	lava.setPosition(0, (WINDOW_Y - 26) * WINDOW_SCALE);
+	lava.setFillColor(sf::Color(248, 32, 0));
+
 	//enemyVec.emplace_back(new Bounder);
 	enemyVec.emplace_back(new Hunter);
 	//enemyVec.emplace_back(new Hunter);
@@ -184,6 +188,7 @@ void Game::nextRound()
 
 void Game::drawTo(sf::RenderWindow& window)
 {
+	window.draw(lava);
 	window.draw(bridge);
 	for (auto& i : platform)
 		i.drawTo(window);
